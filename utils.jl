4587071bce286caa1a7e541f13185906811bc2c2
Matthew Kelley <matthew.curtis.kelley@gmail.com>
2019-11-03 23:44:46 -0700

    Add plotting

--- utils.jl
+++ utils.jl
@@ -1,4 +1,5 @@
-using Flux: onehotbatch
+using Flux: onehotbatch, crossentropy
+using Flux.Tracker: data
 using ProgressBars
 using Random
 using BSON
@@ -8,7 +9,6 @@ const datadir = "sR_bson_subset"
 const PHONES = split("h#	q	eh	dx	iy	r	ey	ix	tcl	sh	ow	z	s	hh	aw	m	t	er	l	w	aa	hv	ae	dcl	y	axr	d	kcl	k	ux	ng	gcl	g	ao	epi	ih	p	ay	v	n	f	jh	ax	en	oy	dh	pcl	ah	bcl	el	zh	uw	pau	b	uh	th	ax-h	em	ch	nx	eng")
 phn2num = Dict(phone=>i for (i, phone) in enumerate(PHONES))
 phn2num["sil"] = 1
-
 
 function loadData()
   Xs, Ys = Vector(), Vector()
@@ -57,4 +57,58 @@ function prepData(Xs, Ys)
   XsBatched = makeBatches(XsShuffled, BATCH_SIZE)
   YsBatched = makeBatches(YsShuffled, BATCH_SIZE)
   return collect(zip(XsBatched, YsBatched))
+end
+
+function calculateCrossEntropy(wordName, model)
+  BSON.@load "$(joinpath(datadir, wordName)).bson" mfccs labs
+  mfccs = [mfccs[i,:] for i=1:size(mfccs, 1)]
+  
+  labs = [phn2num[lab] for lab in vec(labs)]
+  labs = onehotbatch(labs, collect(1:61))
+    
+  labs = [labs[:,i] for i=1:size(labs, 2)]
+  
+  return Float64.(data.(crossentropy.(model.(mfccs), labs)))
+end
+
+function findBoundaries(wordName)
+  BSON.@load "$(joinpath(datadir, wordName)).bson" labs
+  
+  boundaries = [0]
+  curr = labs[1]
+  
+  for (i, l) in enumerate(labs)
+    if l != curr
+      push!(boundaries, i)
+      curr = l
+    end
+  end
+  
+  return boundaries
+end
+
+function transcribe(wordName)
+  BSON.@load "$(joinpath(datadir, wordName)).bson" labs
+  phonemes = [labs[1]]
+  for lab in labs[2:end]
+    if lab != phonemes[length(phonemes)]
+      push!(phonemes, lab)
+    end
+  end
+  
+  return phonemes
+end
+
+function makeTicks(boundaries, labels)
+  ticks = []
+  for (b, l) in zip(boundaries, labels)
+    if l == "sil"
+      l = "#"
+    elseif l == "ao"
+      l = "aa"
+    end
+    push!(ticks, uppercase("$(b)\n$(l)"))
+  end
+  
+  return (boundaries, ticks)
 end
